088a27589c7e5eced383f0ad445d46d5367ec5a2
Victor Vargas <vevargascor@miners.utep.edu>
2020-04-07 17:19:19 -0600

    added username and type from db

--- management-views/employee-view/js/script.js
+++ management-views/employee-view/js/script.js
@@ -5,14 +5,21 @@
         type:'GET',
         dataType:'json',
         success: function(data){
-            console.log(data);
             //Send the user type to the table to provide proper permissions. The type of user is last element in array.
             //configTableProjects(data[data.length-1]);
             const div = document.querySelector('.dropdown-menu');
-            div.innerHTML += `<a class="dropdown-item" href="#">${data[data.length-1]}</a>`;
-            div.innerHTML += `<a class="dropdown-item" href="#">${data[0]}</a>`;
-            div.innerHTML += `<a class="dropdown-item" href="#">Jose Jesus</a>`;
-        }  
+            div.innerHTML += `<a class="dropdown-item" href="#">Type: ${data[data.length-1]}</a>`;
+            div.innerHTML += `<a class="dropdown-item" href="../../../requestForm_Victor/sign-in/index.html">Sign Out</a>`;
+            const drop = document.querySelector('#userProfile');
+            drop.innerHTML += `<svg class="bi bi-person" width="1.5em" height="1.5em" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M15 16s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002zM5.022 15h9.956a.274.274 0 00.014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C13.516 12.68 12.289 12 10 12c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 00.022.004zm9.974.056v-.002zM10 9a2 2 0 100-4 2 2 0 000 4zm3-2a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd" />
+        </svg>
+        ${data[0]}
+        <span class="caret"></span>`
+        },
+        error: function(e){
+            console.log(e.message);
+        }
     });
         
     $.ajax({
@@ -75,7 +82,7 @@ function configTableProjects(){
             mode: "row",
             allowUpdating: false,
             allowDeleting: false,
-            allowAdding: true
+            allowAdding: false
         }, 
         columns: [{
                 dataField: "project",
