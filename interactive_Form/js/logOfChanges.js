326c8490faf65fcfbb72ab28fe03c033c7feca0c
Victor Vargas <vevargascor@miners.utep.edu>
2020-04-20 17:53:51 -0600

    started with log of changes on dx

--- interactive_Form/js/logOfChanges.js
+++ interactive_Form/js/logOfChanges.js
@@ -1,14 +1,18 @@
+(function($){  
+    configureProjectReadinessElementsTable("project-readiness-elements");
+})(jQuery);
+
 var form = [
-{
-    quantity: "",
-    net_year: "",
-    on_state_sys: "",
-    off_state_sys: "",
-    capacity_project: "",
-    numExistingLanes: "",
-    numProjectedLanes: "",
-    miles: ""
-}
+    {
+        quantity: "",
+        net_year: "",
+        on_state_sys: "",
+        off_state_sys: "",
+        capacity_project: "",
+        numExistingLanes: "",
+        numProjectedLanes: "",
+        miles: ""
+    }
 ]
 
 var logOfChanges = []
@@ -28,6 +32,7 @@ function getAllValues() {
         }
     })
     console.log(inputValues);
+    console.log($("#project-readiness-elements").dxDataGrid("instance").getDataSource().items());
     checkForChanges(inputValues);
     form = inputValues;
 }
@@ -42,6 +47,137 @@ function checkForChanges(changes){
             currChanges.push("Change in: " + key)
         }
     }
-    logOfChanges.push(currChanges);
+    if(currChanges.lenght == 1){
+        logOfChanges.push(currChanges);
+    }
+        
     console.log(logOfChanges);
+}
+
+function configureProjectReadinessElementsTable(element){
+    rows = [
+        {
+            "Element":"Schematic"
+        },
+        {
+            "Element":"Env. Doc. Type"
+        },
+        {
+            "Element":"Environmental Doc"
+        },
+        {
+            "Element":"PS&E"
+        },
+        {
+            "Element":"ROW Map(s)"
+        },
+        {
+            "Element":"ROW Acquired"
+        },
+        {
+            "Element":"Utilities"
+        },
+        {
+            "Element":"Public Involvement"
+        },
+        {
+            "Element":"District Review"
+        },
+        {
+            "Element":"Agreement (LPFA)"
+        },
+        {
+            "Element":"Procurement Process"
+        },
+        {
+            "Element":"Let Date"
+        },
+        {
+            "Element":"Construction Performance End Date"
+        },
+        {
+            "Element":"PE Performance End Date"
+        }
+    ]
+
+    progressValues = [
+        "0%",
+        "30%",
+        "60%",
+        "90%",
+        "100%",
+        "N/A"
+    ]
+
+    agencies=[
+        "TxDOT",
+        "Local",
+        "Other"
+    ]
+    $("#"+element).dxDataGrid({
+        dataSource:rows,
+        editing: {
+            mode: "row",
+            allowUpdating: true,
+            allowDeleting: false,
+            allowAdding: false
+        },
+        columnAutoWidth:true,
+        showBorders:true,
+        columns:[
+            {
+                dataField:"Element",
+                allowEditing:false
+            },
+            {
+                dataField:"EstStartDate",
+                dataType:"date",
+                validationRules: [{
+                    type: "required",
+                    message: "Estimated Start Date Required"
+                }]
+            },
+            {
+                dataField:"EstEndDate",
+                dataType:"date",
+                validationRules: [{
+                    type: "required",
+                    message: "Estimated End Date Required"
+                }]
+            },
+            {
+                dataField:"Progress",
+                lookup:{
+                    dataSource:progressValues
+                }
+            },
+            {
+                dataField:"RespAgency",
+                lookup:{
+                    dataSource:agencies
+                }
+            },
+            {
+                dataField:"Comments",
+            }
+        ],
+        onEditorPreparing: function(e) {
+            if((e['row']['data']['Element']==="Let Date"
+            ||  e['row']['data']['Element']==="Construction Performance End Date"
+            ||  e['row']['data']['Element']==="PE Performance End Date") 
+            && (e['dataField']==="Progress" 
+                || e['dataField']==="RespAgency" 
+                || e['dataField']==="Comments")){
+                e.editorOptions.readOnly = true;
+            }
+
+            // e['dataField'] = "Progress";
+            // e.editorOptions.readOnly = true;
+            // e['dataField'] = "RespAgency";
+            // e.editorOptions.readOnly = true;
+            // e['dataField'] = "Progress";
+            // e.editorOptions.readOnly = true;
+        }
+    });
+
 }
