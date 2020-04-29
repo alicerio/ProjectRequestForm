f672b39bb98ac6e5db22aae4fd0bf581708704cb
Victor Vargas <vevargascor@miners.utep.edu>
2020-04-28 22:03:27 -0600

    added all information retrievals, not tested

--- interactive_Form/js/post_data.js
+++ interactive_Form/js/post_data.js
@@ -14,7 +14,7 @@ var project_readiness_elements_footer = JSON.stringify($("#project-readiness-ele
  */
 var project_phases_radios = [
     {
-        "value":""
+        value:""
     }
 ]
 var  inputValues = []
@@ -32,5 +32,65 @@ var  inputValues = []
     })
 project_phases_radios = inputValues;
 
+/**
+ * Here we get from the project phases and type of projects which are form and a datagrid
+ */
 var project_phases = JSON.stringify($("#project-phases").dxForm("instance").getDataSource().items());
 var type_project = JSON.stringify($("#type-project").dxDataGrid("instance").getDataSource().items());
+
+/**
+ * Here we get the inputs from the project cost footer
+ */
+var project_cost_footer=[
+    {
+        is_sponsor_paying_PE:"",
+        is_sponsor_paying_row:"",
+        is_decision_commited:"",
+        percentages:""
+
+    }
+]
+var  inputValues = []
+    $("#project-cost-footer :input").map(function() {
+        var type = $(this).prop("type");
+
+        // checked radios/checkboxes
+        if ((type == "checkbox" || type == "radio")) { 
+            inputValues[$(this).attr("id")] = $(this).is(':checked');
+        }
+        // all other fields, except buttons
+        else if (type != "button" && type != "submit") {
+            inputValues[$(this).attr("id")] = $(this).val();
+        }
+    })
+project_cost_footer = inputValues;
+
+/**
+ * Here we get the info of the values pre project funding
+ */
+var pre_project_funding=[
+    {
+        requesting_mpo_funds:"",
+        YOE_cost:""
+    }
+]
+var  inputValues = []
+    $("#pre-project-funding :input").map(function() {
+        var type = $(this).prop("type");
+
+        // checked radios/checkboxes
+        if ((type == "checkbox" || type == "radio")) { 
+            inputValues[$(this).attr("id")] = $(this).is(':checked');
+        }
+        // all other fields, except buttons
+        else if (type != "button" && type != "submit") {
+            inputValues[$(this).attr("id")] = $(this).val();
+        }
+    })
+    pre_project_funding = inputValues;
+
+    /**
+     * We get project funding values and contact information values
+     */
+var type_project = JSON.stringify($("#project-funding").dxDataGrid("instance").getDataSource().items());
+var contact_info = JSON.stringify($("#contact-information").dxDataGrid("instance").getDataSource().items());
